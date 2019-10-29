1a00463a58c0da3b809201f3007fd00d177e1eda
klau2000 <klau00@stuy.edu>
2019-10-29 15:31:06 -0400

    Initial commit

--- myTunes.c
+++ myTunes.c
@@ -8,3 +8,42 @@ struct song_node{
   char artist[100];
   struct song_node *next;
 };
+
+void print_list(struct song_node *x) {
+    if (x == NULL) {
+      printf("[ ]\n");
+      return;
+    }
+    printf("[ ");
+    struct song_node *temp = x -> next;
+    if (temp == NULL) printf("");
+    else {
+      printf("%s ", x->i);
+      while(temp != NULL) {
+          printf("%s ", temp->i);
+          temp = temp -> next;
+        }
+      }
+    printf("]\n");
+}
+
+struct song_node * insert_front(struct song_node *x, int y) {
+    struct node *temp = malloc(sizeof(struct song_node));
+    temp -> i = y;
+    temp -> next = x;
+    return temp;
+}
+
+int main() {
+    struct song_node *table[27] = calloc (27, sizeof(struct song_node));
+    printf("Testing print_list:\n");
+    table[11] -> name = "Talk";
+    table[11] -> artist = "Khalid";
+    print_list(table);
+
+    //printf("Adding #s 0-9 t list:\n");
+    //for (int i = 0; i < 10; i++) {
+    //  run = insert_front(run, i);
+    //}
+    //print_list(table);
+  }
