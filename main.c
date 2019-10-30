27c345a7cb88c682b5d50071c173f2d42824a54f
emz1003 <emzhang1003@gmail.com>
2019-10-30 14:45:27 -0400

    organize a bit, still have to finish makefile

--- main.c
+++ main.c
@@ -2,3 +2,12 @@
 #include <stdlib.h>
 #include "song_node.h"
 #include "my_tunes.h"
+int main()
+{
+    struct song_node *table[27];
+    // table =  malloc(27 *sizeof(struct song_node));
+    printf("Testing print_list:\n");
+    insert_front(table[11], "Khalid", "Talk");
+    insert_front(table[1], "Avicii", "Levels");
+    print_list(table);
+}
--- song_node.c
+++ song_node.c
@@ -2,3 +2,40 @@
 #include <stdlib.h>
 #include "song_node.h"
 #include "my_tunes.h"
+
+void print_list(struct song_node *x)
+{
+    if (x == NULL)
+    {
+        printf("[ ]\n");
+        return;
+    }
+    printf("[ ");
+    for (int i = 0; i < sizeof(x); i++)
+    {
+        struct song_node *temp = (x + i)->next;
+        if (temp == NULL)
+            printf("");
+        else
+        {
+            printf("%s %s| ", (x + i)->artist, (x + i)->name);
+            while (temp != NULL)
+            {
+                printf("%s %s| ", temp->artist, temp->name);
+                temp = temp->next;
+            }
+        }
+    }
+    printf("]\n");
+}
+
+struct song_node *insert_front(struct song_node *x, char *art, char *na)
+{
+    struct song_node *temp = malloc(sizeof(struct song_node));
+    strcpy(art, temp->artist);
+    strcpy(na, temp->name);
+    //temp -> artist = art;
+    //temp -> name = na;
+    temp->next = x;
+    return temp;
+}
