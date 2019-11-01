8c670634cbb2b33ffd9b8f66966dd23c7510887c
klau2000 <klau00@stuy.edu>
2019-11-01 15:22:49 -0400

    Functions added

--- main.c
+++ main.c
@@ -11,4 +11,20 @@ int main()
     table[1] = insert_front(table[1], "Avicii", "Levels");
     print_list(table[1]);
     print_list(table[11]);
+
+    printf("Testing find node:\n");
+    printf("Looking for Avicii: Levels...\n");
+    if(find_node(table[1], "Avicii", "Levels")){
+        printf("Node Found! %s: %s\n", find_node(table[1], "Avicii", "Levels")-> artist, find_node(table[1], "Avicii", "Levels")-> name);
+    }
+    else {
+        printf("Node Not Found\n");
+    }
+    printf("Looking for Avicii: Wake Me Up...\n");
+    if(find_node(table[1], "Avicii", "Wake Me Up")){
+        printf("Node Found! %s: %s\n", find_node(table[1], "Avicii", "Wake Me Up")-> artist, find_node(table[1], "Avicii", "Wake Me Up")-> name);
+    }
+    else {
+        printf("Node Not Found\n");
+    }
 }
