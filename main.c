3825bc9e5cdd92197a8f85d53908bea4b3bc1574
emz1003 <emzhang1003@gmail.com>
2019-11-03 13:52:37 -0500

    edit testing output

--- main.c
+++ main.c
@@ -8,23 +8,42 @@ int main()
 {
     clear();
     srand(time(NULL));
-    printf("Testing print_list:\n");
-    /* test insert_front */
+    
 
+    /* test insert front */
+
+    printf("Testing insert front:\n");
     table[10] = init_song_node("khalid","talk",NULL);
     table[10] = insert_front(table[10], "katy perry", "harleys in hawaii");
 
     table[0] = init_song_node("avicii", "waiting for love", NULL);
     table[0] = insert_front(table[0],"avicii", "levels");
+
+    /* test insert order */
+    print_list(table[10]);
+    printf("Inserting katy perry: never really over node:\n");\
+    table[10] = insert_order(table[10], "katy perry", "never really over");
+    print_list(table[10]);
+
+    printf("Inserting khalid: walk into the song node:\n");
+    table[10] = insert_order(table[10], "khalid", "walk into the song");
+    print_list(table[10]);
+
+    printf("Inserting katy perry: firework node:\n");
+    table[10] = insert_order(table[10], "katy perry", "firework");
+    print_list(table[10]);
+
+    printf("Inserting kelly clarkson: catch my breath node:\n");
+    table[10] = insert_order(table[10], "kelly clarkson", "catch my breath");
+    print_list(table[10]);
+
+    /* test print list */
+    printf("Testing print_list:\n");
     print_list(table[0]);
     print_list(table[10]);
     printf("\n");
 
-    /* test find_first */
-    printf("Find first song by avicii: %s\n", find_first(table[0], "avicii"));
-    printf("\n");
-
-    /* test find_node */
+    /* test find node */
     printf("Testing find node:\n");
     printf("Looking for avicii: levels...\n");
     struct song_node * temp = find_node(table[0], "avicii", "levels");
@@ -43,26 +62,9 @@ int main()
     }
     printf("\n");
 
-    /* test insert_order */
-    print_list(table[10]);
-    printf("Inserting katy perry: never really over node:\n");\
-    table[10] = insert_order(table[10], "katy perry", "never really over");
-    print_list(table[10]);
-    printf("\n");
-
-    printf("Inserting khalid: walk into the song node:\n");
-    table[10] = insert_order(table[10], "khalid", "walk into the song");
-    print_list(table[10]);
-    printf("\n");
-
-    printf("Inserting katy perry: firework node:\n");
-    table[10] = insert_order(table[10], "katy perry", "firework");
-    print_list(table[10]);
-    printf("\n");
-
-    printf("Inserting kelly clarkson: catch my breath node:\n");
-    table[10] = insert_order(table[10], "kelly clarkson", "catch my breath");
-    print_list(table[10]);
+    /* test find first */
+    printf("Testing find first:\n");
+    printf("Find first song by avicii: %s\n", find_first(table[0], "avicii"));
     printf("\n");
 
     /* test random element */
@@ -76,7 +78,6 @@ int main()
     printf("Removing katy perry: firework node:\n");
     table[10] = remove_node(table[10], find_node(table[10], "katy perry", "firework"));
     print_list(table[10]);
-    printf("\n");
 
     printf("Removing khalid: walk into the song node:\n");
     table[10] = remove_node(table[10], find_node(table[10], "khalid", "walk into the song"));
@@ -88,7 +89,6 @@ int main()
     printf("Freeing table[0] (a):");
     table[0] = free_list(table[0]);
     print_list(table[0]);
-    printf("\n");
 
     print_list(table[10]);
     printf("Freeing table[10] (k):");
@@ -97,7 +97,8 @@ int main()
     printf("\n");
 
     /* test add song node */
-    printf("Testing add_song_node by adding a new song_node with artist bazzi:\n");
+    printf("Testing add_song_node:\n");
+    printf("adding a new song_node with artist bazzi:");
     add_song_node("bazzi", "mine");
     printf("Printing table[1]:\n");
     print_list(table[1]);
@@ -106,12 +107,14 @@ int main()
     printf("Added a new bazzi song:\n");
     add_song_node("bazzi", "beautiful");
     print_list(table[1]);
-    printf("\n");
 
-    printf("Testing print_letter:\n");
+    printf("adding post malone songs:");
     add_song_node("post malone", "sunflower");
     add_song_node("post malone", "circles");
     add_song_node("post malone", "better now");
+    printf("\n");
+
+    printf("Testing print_letter:\n");
     print_letter('p');
 
     /* testing find song */
@@ -130,18 +133,22 @@ int main()
         printf("song found! %s : %s\n", dejavu->artist, dejavu->song);
     else
         printf("song not found\n");
+    printf("\n");
     
     /* testing find artist */
+    printf("Testing find artist:\n");
 
+    printf("\n");
     /* testing print letter */
-
+    printf("\n");
     /* testing print artist */
-
+    printf("\n");
     /* testing print all */
-
+    printf("\n");
     /* testing shuffle */
-
+    printf("\n");
     /* testing delete song */
-
+    printf("\n");
     /* testing clear */
+    printf("\n");
 }
